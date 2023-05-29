ff51aca5f16c249a0260ee351cb7f298a5849308
Minori-Takeuchi <shato.min0716@gmeail.com>
2023-05-29 15:27:50 +0900

    setting firebase

---
+++ src/components/MainHeader.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="header">
+    <button v-if="user" @click="logout">ろぐあうと</button>
+  </div>
+</template>
+
+<script>
+import { auth } from "../firebase";
+import { useStore } from "vuex";
+import { computed } from "vue";
+
+export default {
+  name: "MainHeader",
+  setup() {
+    const store = useStore();
+    const user = computed(() => store.state.user);
+    const logout = async () => {
+      try {
+        auth.signOut().then(() => {
+          store.commit("setUser", null);
+          alert("ログアウトしました");
+        });
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    return {
+      logout,
+      user
+    };
+  },
+};
+</script>
